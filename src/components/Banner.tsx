ae7adf32cf7a426770e77273f52c987ab99bfe9a
Víctor Quiñones <victhorq716@gmail.com>
2024-12-17 13:35:47 -0400

    add banner-gif

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -27,7 +27,7 @@ const Banner = () => {
             height={100}
             alt='gif'
             className='w-full h-full object-cover'
-            src='https://64.media.tumblr.com/cb1a6d28f1f97fe56c0764cdf40fc92c/d662df3b2e19bcdc-c5/s2048x3072/be56de3866ebc146e4f1b0893ca69b955a92ae23.gifv'
+            src='/images/banner-gif.gif'
           />
         </div>
       </div>
